88ee99c170f1b7acb5f1cf95b69602780d374aba
Laura Costa Rocha <100743359+delaureanlaura@users.noreply.github.com>
2022-09-21 11:49:53 -0300

    Update forca.py

--- forca.py
+++ forca.py
@@ -91,39 +91,6 @@ def ganhador():
         print("         _.' '._        ")
         print("        '-------'       ")
 
-def perdedor(palavra_secreta):
-    print("Puxa, você foi enforcado!")
-    print("A palavra era {}".format(palavra_secreta))
-    print("    _______________         ")
-    print("   /               \       ")
-    print("  /                 \      ")
-    print("//                   \/\  ")
-    print("\|   XXXX     XXXX   | /   ")
-    print(" |   XXXX     XXXX   |/     ")
-    print(" |   XXX       XXX   |      ")
-    print(" |                   |      ")
-    print(" \__      XXX      __/     ")
-    print("   |\     XXX     /|       ")
-    print("   | |           | |        ")
-    print("   | I I I I I I I |        ")
-    print("   |  I I I I I I  |        ")
-    print("   \_             _/       ")
-    print("     \_         _/         ")
-    print("       \_______/           ")
-
-def ganhador():
-    def imprime_mensagem_vencedor():
-        print("Parabéns, você ganhou!")
-        print("       ___________      ")
-        print("      '._==_==_=_.'     ")
-        print("      .-\\:      /-.    ")
-        print("     | (|:.     |) |    ")
-        print("      '-|:.     |-'     ")
-        print("        \\::.    /      ")
-        print("         '::. .'        ")
-        print("           ) (          ")
-        print("         _.' '._        ")
-        print("        '-------'       ")
 
 def desenha_forca(tentativas):
     print("  _______     ")
